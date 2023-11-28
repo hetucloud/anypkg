c7fd25e21263668615848dc16a39760fb1f0a9d4
xujiang <realjiangxu@gmail.com>
2023-11-28 23:08:58 +0800

    feat: Hadoop parcel add install script.

--- bigtop-packages/src/parcel/hadoop/SPACES/hadoop_parcel.sh
+++ bigtop-packages/src/parcel/hadoop/SPACES/hadoop_parcel.sh
@@ -84,7 +84,16 @@ PKG_NAME="hadoop"
 etc_default="/etc/default"
 # TODO: hadoop home dir
 usr_lib_hadoop="${INSTALL_PATH}/${PKG_NAME}"
+usr_lib_hdfs=${INSTALL_PATH}/${PKG_NAME}-hdfs
+usr_lib_yarn=${INSTALL_PATH}/${PKG_NAME}-yarn
+usr_lib_mapreduce=${INSTALL_PATH}/${PKG_NAME}-mapreduce
+var_lib_hdfs=${INSTALL_PATH}/${PKG_NAME}-hdfs
+var_lib_mapreduce=${INSTALL_PATH}/${PKG_NAME}-mapreduce
+var_lib_httpfs=${INSTALL_PATH}/${PKG_NAME}-httpfs
+var_lib_kms=${INSTALL_PATH}/${PKG_NAME}-kms
 etc_hadoop_conf_dist="${INSTALL_PATH}/etc/${PKG_NAME}/conf.dist"
+
+usr_lib_zookeeper=${INSTALL_PATH}/zookeeper
 
 bin_dir="${usr_lib_hadoop}/bin"
 man_dir="${usr_lib_hadoop}/man"
@@ -93,13 +102,12 @@ include_dir="/usr/include"
 lib_dir="/usr/lib"
 doc_hadoop="${doc_dir}"
 
-# For examples: ROOT_DIR = /ws/bigtop/build/hadoop/parcel
+# For examples: ROOT_DIR = /ws/anypkg/build/hadoop/parcel
 PARCELS_DIR="${ROOT_DIR}/PARCELS"
 SPARCELS_DIR="${ROOT_DIR}/SPARCELS"
 PARCEL_SOURCE_DIR="${ROOT_DIR}/SOURCES"
 PARCEL_BUILD_ROOT="${ROOT_DIR}/BUILD"
 PARCEL_INSTALL_PREFX="${ROOT_DIR}/INSTALL"
-
 
 # Build
 tar -zxvf $SPARCELS_DIR/${PKG_NAME}*-$PKG_VERSION.$STACK_VERSION-$BUILD_NUMBER.src.parcel -C $PARCEL_BUILD_ROOT  
@@ -116,23 +124,35 @@ bash ../do-component-build
 popd
 
 # Install
-rm -rf "$PARCEL_INSTALL_PREFX/*"
-# pushd $PARCEL_BUILD_ROOT/$PKG_NAME-$PKG_VERSION-src 
-# bash $PARCEL_SOURCE_DIR/install_hadoop.sh \
-#           --build-dir=build \
-#           --prefix=$PARCEL_INSTALL_PREFX \
-#           --doc-dir=${doc_hadoop} \
-#           --lib-dir=${usr_lib_hadoop} \
-#           --bin-dir=${bin_dir} \
-#           --man-dir=${man_dir} \
-#           --conf-dist-dir=${etc_hadoop_conf_dist} \
-#           --etc-default=${etc_default} \
-#           --system-include-dir=${include_dir} \
-#           --system-lib-dir=${lib_dir}
-# popd
+rm -rf $PARCEL_INSTALL_PREFX/*
+pushd $PARCEL_BUILD_ROOT/$PKG_NAME-$PKG_VERSION-src 
+bash $PARCEL_SOURCE_DIR/install_hadoop.sh \
+  --distro-dir=$PARCEL_SOURCE_DIR \
+  --build-dir=$PWD/build \
+  --prefix=$PARCEL_INSTALL_PREFX \
+  --doc-dir=${doc_hadoop} \
+  --bin-dir=${bin_dir} \
+  --man-dir=${man_dir} \
+  --etc-default=${etc_default} \
+  --hadoop-dir=${usr_lib_hadoop} \
+  --hdfs-dir=${usr_lib_hdfs} \
+  --yarn-dir=${usr_lib_yarn} \
+  --mapreduce-dir=${usr_lib_mapreduce} \
+  --var-hdfs=${var_lib_hdfs} \
+  --var-yarn=${var_lib_uarn} \
+  --var-mapreduce=${var_lib_mapreduce} \
+  --var-httpfs=${var_lib_httpfs} \
+  --var-kms=${var_lib_kms} \
+  --system-include-dir=${include_dir} \
+  --system-lib-dir=${lib_dir} \
+  --etc-hadoop=${etc_hadoop}
+popd
+
+# Forcing Zookeeper dependency to be on the packaged jar
+# TODO ln -s ${usr_lib_zookeeper}/zookeeper.jar $PARCEL_BUILD_ROOT/${usr_lib_hadoop}/lib/zookeeper-[[:digit:]]*.jar
 
 # Generate parcel pkg
-# tar -czvf $PARCELS_DIR/"$PKG_NAME"_"$PKG_VERSION"-"$STACK_VERSION".parcel  -C $PARCEL_INSTALL_PREFX .
+tar -czvf $PARCELS_DIR/"$PKG_NAME"_"$PKG_VERSION"-"$STACK_VERSION".parcel  -C $PARCEL_INSTALL_PREFX .
 
 # Docker build image
 # Note: That podman needs to be run as a non-root user
